2fdba96a999d9549a465817d2af6838fd3fdb4f5
Alex Lange <lange.alexander222@gmail.com>
2020-06-03 11:36:42 -0700

    Configure org-agenda, capture, and refile

--- init.el
+++ init.el
@@ -26,3 +26,17 @@
  '(package-selected-packages
    (quote
     (org-bullets smart-mode-line solarized-theme helm-projectile helm org-plus-contrib use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka" :height 1.3))))
+ '(org-level-2 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka" :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka" :height 1.1))))
+ '(org-level-4 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka" :height 1.05))))
+ '(org-level-5 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka"))))
+ '(org-level-6 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka"))))
+ '(org-level-7 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka"))))
+ '(org-level-8 ((t (:inherit default :weight demibold :foreground "#839496" :font "Iosevka")))))
