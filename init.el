acd0d4efcc8e5cd9dc8b821e0248f066c195a3d3
Alex Lange <lange.alexander222@gmail.com>
2021-02-15 12:45:21 -0500

    Add initial config for org-journal

--- init.el
+++ init.el
@@ -24,8 +24,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (org-bullets smart-mode-line solarized-theme helm-projectile helm org-plus-contrib use-package))))
+   '(org-bullets smart-mode-line solarized-theme helm-projectile helm org-plus-contrib use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
